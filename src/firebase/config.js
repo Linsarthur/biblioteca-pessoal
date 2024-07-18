44a7af81c899531f5fecad3c09ebebab72c4b1e3
Linsarthur <linsarthur02@gmail.com>
2024-07-18 10:40:45 -0300

    Inclusão dos arquivos base do projeto de firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgaUDxbBwqmE9wxTtoIf8T7M4eHWD7fRo",
+  authDomain: "semanal-crud.firebaseapp.com",
+  projectId: "semanal-crud",
+  storageBucket: "semanal-crud.appspot.com",
+  messagingSenderId: "448213218946",
+  appId: "1:448213218946:web:2c9e1b99bc8eec7096269b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
