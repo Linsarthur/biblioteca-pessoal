670c441d839bcac418c0a6606558ae272014daff
flaviare1s <flaviarster@gmail.com>
2024-07-22 12:52:50 -0300

    Adicionei span de Descrição e coloquei as chaves de ambos os bancos no config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,21 +3,22 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // const firebaseConfig = {
-//   apiKey: "AIzaSyBgaUDxbBwqmE9wxTtoIf8T7M4eHWD7fRo",
-//   authDomain: "semanal-crud.firebaseapp.com",
-//   projectId: "semanal-crud",
-//   storageBucket: "semanal-crud.appspot.com",
-//   messagingSenderId: "448213218946",
-//   appId: "1:448213218946:web:2c9e1b99bc8eec7096269b"
+//   apiKey: "AIzaSyCFosruYv8OUzJpyqsFW3YwUmnA6rWmBwk",
+//   authDomain: "projeto-reserva-21deb.firebaseapp.com",
+//   projectId: "projeto-reserva-21deb",
+//   storageBucket: "projeto-reserva-21deb.appspot.com",
+//   messagingSenderId: "144217242525",
+//   appId: "1:144217242525:web:4af3ae3de7f5d9212bcd74"
 // };
 
+// BANCO ANTIGO:
 const firebaseConfig = {
-  apiKey: "AIzaSyCFosruYv8OUzJpyqsFW3YwUmnA6rWmBwk",
-  authDomain: "projeto-reserva-21deb.firebaseapp.com",
-  projectId: "projeto-reserva-21deb",
-  storageBucket: "projeto-reserva-21deb.appspot.com",
-  messagingSenderId: "144217242525",
-  appId: "1:144217242525:web:4af3ae3de7f5d9212bcd74"
+  apiKey: "AIzaSyBgaUDxbBwqmE9wxTtoIf8T7M4eHWD7fRo",
+  authDomain: "semanal-crud.firebaseapp.com",
+  projectId: "semanal-crud",
+  storageBucket: "semanal-crud.appspot.com",
+  messagingSenderId: "448213218946",
+  appId: "1:448213218946:web:2c9e1b99bc8eec7096269b"
 };
 
 export const app = initializeApp(firebaseConfig);
