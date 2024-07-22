1848b5fbdf23b6f0d97b1d333fcf49cd1367d22f
flaviare1s <flaviarster@gmail.com>
2024-07-22 13:22:53 -0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,16 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCFosruYv8OUzJpyqsFW3YwUmnA6rWmBwk",
-//   authDomain: "projeto-reserva-21deb.firebaseapp.com",
-//   projectId: "projeto-reserva-21deb",
-//   storageBucket: "projeto-reserva-21deb.appspot.com",
-//   messagingSenderId: "144217242525",
-//   appId: "1:144217242525:web:4af3ae3de7f5d9212bcd74"
-// };
-
-// BANCO ANTIGO:
+// BANCO  OFICIAL:
 const firebaseConfig = {
   apiKey: "AIzaSyBgaUDxbBwqmE9wxTtoIf8T7M4eHWD7fRo",
   authDomain: "semanal-crud.firebaseapp.com",
@@ -20,6 +11,16 @@ const firebaseConfig = {
   messagingSenderId: "448213218946",
   appId: "1:448213218946:web:2c9e1b99bc8eec7096269b"
 };
+
+// BANCO DE TESTE:
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCFosruYv8OUzJpyqsFW3YwUmnA6rWmBwk",
+//   authDomain: "projeto-reserva-21deb.firebaseapp.com",
+//   projectId: "projeto-reserva-21deb",
+//   storageBucket: "projeto-reserva-21deb.appspot.com",
+//   messagingSenderId: "144217242525",
+//   appId: "1:144217242525:web:4af3ae3de7f5d9212bcd74"
+// };
 
 export const app = initializeApp(firebaseConfig);
 
