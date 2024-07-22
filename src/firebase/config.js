7164139787e01a91ec8f2b31c45742965914951b
flaviare1s <flaviarster@gmail.com>
2024-07-22 13:35:00 -0300

    Troquei filtragem de botão para checkbox

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,24 +3,24 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // BANCO  OFICIAL:
-const firebaseConfig = {
-  apiKey: "AIzaSyBgaUDxbBwqmE9wxTtoIf8T7M4eHWD7fRo",
-  authDomain: "semanal-crud.firebaseapp.com",
-  projectId: "semanal-crud",
-  storageBucket: "semanal-crud.appspot.com",
-  messagingSenderId: "448213218946",
-  appId: "1:448213218946:web:2c9e1b99bc8eec7096269b"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBgaUDxbBwqmE9wxTtoIf8T7M4eHWD7fRo",
+//   authDomain: "semanal-crud.firebaseapp.com",
+//   projectId: "semanal-crud",
+//   storageBucket: "semanal-crud.appspot.com",
+//   messagingSenderId: "448213218946",
+//   appId: "1:448213218946:web:2c9e1b99bc8eec7096269b"
+// };
 
 // BANCO DE TESTE:
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCFosruYv8OUzJpyqsFW3YwUmnA6rWmBwk",
-//   authDomain: "projeto-reserva-21deb.firebaseapp.com",
-//   projectId: "projeto-reserva-21deb",
-//   storageBucket: "projeto-reserva-21deb.appspot.com",
-//   messagingSenderId: "144217242525",
-//   appId: "1:144217242525:web:4af3ae3de7f5d9212bcd74"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyCFosruYv8OUzJpyqsFW3YwUmnA6rWmBwk",
+  authDomain: "projeto-reserva-21deb.firebaseapp.com",
+  projectId: "projeto-reserva-21deb",
+  storageBucket: "projeto-reserva-21deb.appspot.com",
+  messagingSenderId: "144217242525",
+  appId: "1:144217242525:web:4af3ae3de7f5d9212bcd74"
+};
 
 export const app = initializeApp(firebaseConfig);
 
